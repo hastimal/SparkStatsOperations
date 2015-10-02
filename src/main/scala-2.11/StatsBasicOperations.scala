79b8761cc4da13cd721fed619e455347812a4f93
hastimal <hjdk4@mail.umkc.edu>
2015-10-01 20:39:29 -0500

    Added mode and median too

--- src/main/scala-2.11/StatsBasicOperations.scala
+++ src/main/scala-2.11/StatsBasicOperations.scala
@@ -37,8 +37,48 @@ object StatsBasicOperations  {
     println("Variance of numbers is    "+variance)
     val std = stats.variance
     println("Standard deviation is    "+std)
+    val arry = lines.toArray()
+    val sorted = scala.util.Sorting.quickSort(arry)
+    //finding median
+    var median: Double = 0.0
+    val middle = ((arry.length) / 2)
+    if(arry.length % 2 == 0){
+      val medianA = arry(middle)
+      val medianB = arry(middle-1)
+      median = (medianA + medianB).toFloat / 2
+    } else{
+      median = arry(middle + 1).toFloat
+    }
+    println("Median of numbers is      "+median)
 
+    //finding mode
+    var t = 0;
+      for( i <- 0 until arry.length){
+        for( j <- 1 until arry.length - i){
+        if(arry(j-1) > arry(j)){
+            t = arry(j-1)
+            arry(j-1) = arry(j)
+            arry(j) = t
+        }
+      }
+    }
 
+    var mode = arry(0)
+    var temp = 1
+    var temp2 = 1
+    for( i <- 1 until arry.length){
+      if(arry(i-1) == arry(i)){
+        temp = temp + 1
+      }
+      else {
+        temp = 1
+      }
+      if(temp >= temp2){
+        mode = arry(i)
+        temp2 = temp
+      }
+    }
+    println("Mode of numbers is       "+mode)
    //Stopping spark
     sc.stop()
 
